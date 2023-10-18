b37225196780738e958351845b8d6f0ce49c6e07
Philip Fourie <philip.fourie@theta.co.nz>
2023-10-19 08:27:50 +1300

    feat: Oil plugin added

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+
+		config = function()
+			require("oil").setup({})
+
+			vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
+}
